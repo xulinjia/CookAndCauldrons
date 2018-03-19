f9b6a6b59043258bf25bbb13df590731b5dfa84c
xulinjia <779197295@qq.com>
2018-03-19 22:55:21 +0800

    roll

---
+++ Cook/Assets/Script/Main/WheelRoll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelRoll : MonoBehaviour {
+
+    public GameObject road;
+    public float speed = 1;
+    public float sum = 0f;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        road.transform.Translate(Vector3.left * speed);
+        //road.transform.localPosition = new Vector3(road.transform.localPosition.x - 0.1f, road.transform.localPosition.y, road.transform.localPosition.z);
+        Debug.Log(Vector3.left * speed);
+	}
+}
